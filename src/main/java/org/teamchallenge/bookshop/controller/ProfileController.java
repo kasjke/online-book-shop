9c0cf4fbbdc55d355ec79a51410c24deb583065b
Pavlo Polezhay <pasha200417@gmail.com>
2024-08-17 10:34:10 +0300

    Updated

--- src/main/java/org/teamchallenge/bookshop/controller/ProfileController.java
+++ src/main/java/org/teamchallenge/bookshop/controller/ProfileController.java
@@ -3,13 +3,14 @@ package org.teamchallenge.bookshop.controller;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.teamchallenge.bookshop.dto.BookDto;
 import org.teamchallenge.bookshop.dto.ProfileUpdateDto;
+import org.teamchallenge.bookshop.mapper.ProfileMapper;
 import org.teamchallenge.bookshop.model.Profile;
+import org.teamchallenge.bookshop.model.User;
 import org.teamchallenge.bookshop.service.ProfileService;
+import org.teamchallenge.bookshop.service.UserService;
 
 @RestController
 @RequestMapping("api/v1/profile")
@@ -18,9 +19,13 @@ import org.teamchallenge.bookshop.service.ProfileService;
 @Tag(name = "Profile controller", description = "API for profile management")
 public class ProfileController {
     private final ProfileService profileService;
+    private final ProfileMapper profileMapper;
+    private final UserService userService;
 
-    @PatchMapping("update/{id}")
-    public ResponseEntity<Profile> updateProfile(@PathVariable Long id, @RequestBody ProfileUpdateDto profileUpdateDto) {
+    @PatchMapping("update")
+    public ResponseEntity updateProfile(@RequestBody ProfileUpdateDto profileUpdateDto) {
+        User user = userService.getAuthenticatedUser();
+        Long id = user.getProfile().getId();
         Profile updatedProfile = profileService.updateProfile(id, profileUpdateDto);
         return ResponseEntity.ok(updatedProfile);
     }
--- src/main/java/org/teamchallenge/bookshop/model/Profile.java
+++ src/main/java/org/teamchallenge/bookshop/model/Profile.java
@@ -1,8 +1,15 @@
 package org.teamchallenge.bookshop.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Table(name = "profiles")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Profile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
